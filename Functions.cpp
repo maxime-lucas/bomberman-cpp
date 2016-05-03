9ddb7846165a0c1b0dedb7c6ced9090cc2e5c41e
Maxime <maxime.lucas96@gmail.com>
2016-05-03 19:23:52 +0200

    Nettoyage de printemps

--- Functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "Header.h"
-
-void apply_surface( int x, int y , SDL_Surface *src, SDL_Surface *dest, SDL_Rect *clip)
-{
-    SDL_Rect offset;
-    offset.x = x;
-    offset.y = y;
-    SDL_BlitSurface(src , clip , dest , &offset);
-}
