3a22630d24ac5ff4ca1f3a8a0f3903eaebb48e7a
Benjamin Anselin <b1jam1.ansel1@gmail.com>
2016-05-03 19:22:52 +0200

    on a trouvé ce foutu bug avec le Functions.h

--- Functions.cpp
+++ Functions.cpp
@@ -1,9 +1,12 @@
 #include "Header.h"
 
-void apply_surface( int x, int y , SDL_Surface *src, SDL_Surface *dest, SDL_Rect *clip)
+void apply_surface( int x, int y, SDL_Surface* src, SDL_Surface* dest, SDL_Rect* clip = NULL )
 {
     SDL_Rect offset;
+
     offset.x = x;
     offset.y = y;
-    SDL_BlitSurface(src , clip , dest , &offset);
+
+    //On blitte la surface
+    SDL_BlitSurface( src, clip, dest, &offset );
 }
