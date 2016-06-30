011e48f81220ff9ab54b239b6df64554304b3030
Maxime <maxime.lucas96@gmail.com>
2016-06-30 22:31:29 +0200

    Tout remarche avec Joystick

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -1,47 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Animation.cpp
  * Author: maxime
+ * 
+ * Composition :
+ * - Forme canonique (Constructeur, Destructeur, Recopie, Surcharge=)
  * 
  * Created on 13 mai 2016, 12:47
  */
 
 #include "Header.h"
+
+/**
+ * Constructeur par défaut
+ * - Place l'image actuelle au début (0)
+ */
 Animation::Animation()
 {
+    this->path = "";
+    this->nbFrames = 0;
     this->currentFrame = 0;
 }
 
-Animation::Animation(char* str, int nb)
+/**
+ * Constructeur paramétré
+ * @param str   Chemin vers l'image
+ * @param nb    Nombre d'images de l'animation
+ */
+Animation::Animation(string str, int nb)
 {
     this->nbFrames = nb;
     this->path = str;
     this->currentFrame = 0;
 }
 
+Animation::Animation( const Animation &a )
+{
+    this->nbFrames = a.nbFrames;
+    this->path = a.path;
+    this->currentFrame = 0;       
+}
+
+/**
+ * Destructeur
+ * - Vide l'image stockée dans'img'
+ */
 Animation::~Animation()
 {
     SDL_FreeSurface(img);
 }
 
+/**
+ * Rendu de la frame actuelle
+ * @param ecran Ecran de destination, dans lequel on doit faire le rendu de l'image
+ */
 void Animation::render(SDL_Surface* ecran)
 {
+    // Désallocation de l'image précédente
+    SDL_FreeSurface(img);
+    
+    // Préparation du chemin de l'image
     char filepath[64];
     sprintf(filepath, path.c_str() , currentFrame);
+    
+    // Chargement de l'image
     img = IMG_Load( filepath );
+    
+    // Collage de l'image dans l'écran de destination
     apply_surface( 0, 0, img, ecran, NULL );
 }
 
+/**
+ * Passer à la frame suivante
+ */
 void Animation::nextFrame()
 {
     currentFrame++;
 }
 
+/**
+ * Remise à 0 de la frame actuelle
+ */
 void Animation::resetFrames()
 {
     currentFrame = 0;
--- src/Input.cpp
+++ src/Input.cpp
@@ -3,44 +3,86 @@
 
 Input::Input()
 {
-	memset(this,0,sizeof(*this));
+    // On met à 0 le tableau contenant l'état des touches du clavier
+    memset(this,0,sizeof(*this));
+
+    // Initialisation du nombre de joystick
+    nbJoysticks = SDL_NumJoysticks();
+
+    // Création du tableau de structure de joysticks
+    joysticks = (InputJoystick*) malloc ( nbJoysticks * sizeof(InputJoystick) );
+
+    // Création des joysticks
+    for( int i = 0 ; i < nbJoysticks ; i++ ) {
+        joysticks[i].joystick = SDL_JoystickOpen(i);
+        joysticks[i].id = i;
+        joysticks[i].buttons = (char*) malloc ( SDL_JoystickNumButtons(joysticks[i].joystick) * sizeof(char) );
+        joysticks[i].hats = (int*) malloc ( SDL_JoystickNumHats(joysticks[i].joystick) * sizeof(int) );
+
+        memset( joysticks[i].buttons, 0, SDL_JoystickNumButtons(joysticks[i].joystick) * sizeof(char) );
+        memset( joysticks[i].hats, 0, SDL_JoystickNumHats(joysticks[i].joystick) * sizeof(int) );
+    }
 }
 
-Input::~Input(){ }
+Input::~Input()
+{	
+	
+}
 
-void Input::Update()
+void Input::update()
 {
-	SDL_Event event;
-        mouseButtons[SDL_BUTTON_WHEELUP] = 0;
-        mouseButtons[SDL_BUTTON_WHEELDOWN] = 0;
-	while(SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-		case SDL_KEYDOWN:
-			key[event.key.keysym.sym]=1;
-			break;
-		case SDL_KEYUP:
-			key[event.key.keysym.sym]=0;
-			break;
-		case SDL_MOUSEMOTION:
-			mouseX=event.motion.x;
-			mouseY=event.motion.y;
-			mouseXRel=event.motion.xrel;
-			mouseYRel=event.motion.yrel;
-			break;
-		case SDL_MOUSEBUTTONDOWN:
-			mouseButtons[event.button.button]=1;
-			break;
-		case SDL_MOUSEBUTTONUP:
-                        if (event.button.button!=SDL_BUTTON_WHEELUP && event.button.button!=SDL_BUTTON_WHEELDOWN)
-  			   mouseButtons[event.button.button]=0;
-			break;
-		case SDL_QUIT:
-			quit = 1;
-			break;
-		default:
-			break;
-		}
-	}
+    SDL_Event event;
+    mouseButtons[SDL_BUTTON_WHEELUP] = 0;
+    mouseButtons[SDL_BUTTON_WHEELDOWN] = 0;
+
+    while(SDL_PollEvent(&event))
+    {
+        if( nbJoysticks != 0 )
+        {
+            switch (event.type)
+            {
+                case SDL_JOYBUTTONDOWN:
+                    joysticks[ event.jbutton.which ].buttons[ event.jbutton.button ] = 1;
+                    //cout << "Button : " << (int) event.jbutton.button << endl;
+                    break;
+                case SDL_JOYBUTTONUP:
+                    joysticks[ event.jbutton.which ].buttons[ event.jbutton.button ] = 0;
+                    break;
+                case SDL_JOYHATMOTION:
+                    joysticks[ event.jhat.which ].hats[ event.jhat.hat ] = event.jhat.value;
+                    //cout << "Hat : " << (int) event.jhat.which << " ; Valeur :" << (int) event.jhat.value << endl;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        switch (event.type)
+        {
+            case SDL_KEYDOWN:
+                key[event.key.keysym.sym]=1;
+                break;
+            case SDL_KEYUP:
+                key[event.key.keysym.sym]=0;
+                break;
+            case SDL_MOUSEMOTION:
+                mouseX=event.motion.x;
+                mouseY=event.motion.y;
+                mouseXRel=event.motion.xrel;
+                mouseYRel=event.motion.yrel;
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                mouseButtons[event.button.button]=1;
+                break;
+            case SDL_MOUSEBUTTONUP:
+                if (event.button.button!=SDL_BUTTON_WHEELUP && event.button.button!=SDL_BUTTON_WHEELDOWN)
+                    mouseButtons[event.button.button]=0;
+                break;
+            case SDL_QUIT:
+                quit = 1;
+                break;
+            default:
+                break;
+        }
+    }	
 }
